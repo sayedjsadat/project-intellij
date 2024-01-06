ff44eec65b40ddd379756b9ec1585c949436e994
Jalal <sadat.sayed.3@student.greenriver.edu>
2024-01-06 00:11:11 -0800

    Revert "added a method to print a header."
    This reverts commit cc2e21da

--- src/java1/Main.java
+++ src/java1/Main.java
@@ -3,6 +3,7 @@ package java1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Mars!");
+        System.out.println("By: Sayed");
     }
 
     public static void printMessage(String message)
@@ -10,8 +11,4 @@ public class Main {
         System.out.println(message);
     }
 
-    private static void printHeader()
-    {
-        System.out.println("****************************Jalla*****");
-    }
 }
