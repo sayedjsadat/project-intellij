1daf38417db9cff15ce9efca53ad27a424b86913
Jalal <sadat.sayed.3@student.greenriver.edu>
2024-01-06 00:10:46 -0800

    added a method to print a header.

--- src/java1/Main.java
+++ src/java1/Main.java
@@ -12,6 +12,6 @@ public class Main {
 
     private static void printHeader()
     {
-        System.out.println("*********************************");
+        System.out.println("****************************Jalla*****");
     }
 }
